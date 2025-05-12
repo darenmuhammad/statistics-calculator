71611c56a8fe2c99bc04f4a186802d64041c8511
darenmuhammad <darenmuhammad10@gmail.com>
2025-05-12 22:24:26 +0700

    done the calculation function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # statistics-calculator
+
+1. split
+2. map
+3. filter
+4. isNan
+5. reduce
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const getMean = (array) => {
+    const sum = array.reduce((acc, el) => acc + el, 0);
+    const mean = sum / array.length;
+    return mean;
+};
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 };
