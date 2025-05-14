c3122f3d9b49e38dc37cb4e2841b32b83ed0df2a
darenmuhammad <darenmuhammad10@gmail.com>
2025-05-14 13:33:44 +0700

    adding median and mode

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 3. filter
 4. isNan
 5. reduce
+6. sort
+   The .sort() method mutates the original array - in other words, it modifies the order of the elements directly.
+7. toSorted()
--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const getMean = (array) => {
-    const sum = array.reduce((acc, el) => acc + el, 0);
-    const mean = sum / array.length;
-    return mean;
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+    const sorted = array.toSorted((a, b) => a - b);
+
+    // check sorted's length is even or not
+    const median =
+        sorted.length % 2 === 0
+            ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+            : sorted[Math.floor(sorted.length / 2)];
+    return median;
+};
+
+const getMode = (array) => {
+
 };
 
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+
+    const mean = getMean(numbers);
+    const median = getMedian(numbers);
+
+    document.querySelector("#mean").textContent = mean;
+    document.querySelector("#median").textContent = median;
 };
